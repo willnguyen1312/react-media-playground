b297736402f104109b951856ce3c4836faddff06
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-07-13 09:52:56 +0700

    Add favicon icon 🎉

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 import React from "react";
 
 function App() {
+  const handleOnEnded = (
+    event: React.SyntheticEvent<HTMLVideoElement, Event>
+  ) => {
+    console.log(`Ended event - ended value: ${event.currentTarget.ended}`);
+  };
+
+  const handleOnPause = (
+    event: React.SyntheticEvent<HTMLVideoElement, Event>
+  ) => {
+    console.log(`Pause event - ended value: ${event.currentTarget.ended}`);
+  };
+
+  const handleOnPlay = (
+    event: React.SyntheticEvent<HTMLVideoElement, Event>
+  ) => {
+    console.log(`Play event - ended value: ${event.currentTarget.ended}`);
+  };
+
+  const handleOnSeeking = (
+    event: React.SyntheticEvent<HTMLVideoElement, Event>
+  ) => {
+    console.log(`Seeking event - ended value: ${event.currentTarget.ended}`);
+  };
+
   return (
     <main>
-      <h1>Hello Vite</h1>
+      <video
+        width="800"
+        height="500"
+        onEnded={handleOnEnded}
+        onPause={handleOnPause}
+        onPlay={handleOnPlay}
+        onSeeking={handleOnSeeking}
+        controls
+        src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+      />
     </main>
   );
 }
